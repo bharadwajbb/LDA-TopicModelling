2f0946ecf33a30905f3bc47231220676f5314d0f
bb.bharadwaj1999@gmail.com <bb.bharadwaj1999@gmail.com>
2024-04-25 15:10:37 -0400

    changes to start.py

--- start.py
+++ start.py
@@ -18,40 +18,32 @@ INTERPRETATION = False
 
 
 if __name__ == "__main__":
+	extraction = Extraction()
+	extraction.extract_publications()
 
-	if EXTRACTION:
-		extraction = Extraction()
-		extraction.extract_publications()
-
-
-	if PREPROCESSING:
-		preprocessing = Preprocessing()
-		preprocessing.full_text_preprocessing()
-		preprocessing.general_preprocessing()
+	preprocessing = Preprocessing()
+	preprocessing.full_text_preprocessing()
+	preprocessing.general_preprocessing()
 
 
-	if TRANSFORMATION:
-		transformation = Transformation()
-		transformation.transform_for_lda()
+	transformation = Transformation()
+	transformation.transform_for_lda()
 
 
-	if DATAMINING:
-		datamining = Datamining()
-		datamining.execute_lda()
+	datamining = Datamining()
+	datamining.execute_lda()
 
 
-	if EVALUATION:
-		evaluation = Evaluation()
-		evaluation.calculate_coherence()
-		evaluation.plot_coherence()
-		evaluation.output_lda_topics()
+
+	evaluation = Evaluation()
+	evaluation.calculate_coherence()
+	evaluation.plot_coherence()
+	evaluation.output_lda_topics()
 
 
-	if INTERPRETATION:
-		interpretation = Interpretation()
-		interpretation.infer_document_topic_distribution()
-		interpretation.get_document_title_per_topic()
-		interpretation.plot_topics_over_time()
-		interpretation.plot_topics_over_time_stacked()
-		interpretation.plot_topic_co_occurrence()
-		interpretation.plot_topics_in_journals()
+	interpretation = Interpretation()
+	interpretation.infer_document_topic_distribution()
+	interpretation.get_document_title_per_topic()
+	interpretation.plot_topics_over_time()
+	interpretation.plot_topic_co_occurrence()
+	interpretation.plot_topics_in_journals()
